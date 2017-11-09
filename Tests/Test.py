ae3da9352a08bb20d2e24c458e5ae17bda474a1d
Samuel W. Flint <swflint@flintfam.org>
2017-11-09 07:26:28 -0600

    Updated test library

--- Tests/Test.py
+++ Tests/Test.py
@@ -1,28 +1,61 @@
 #!/bin/python
 # coding: utf-8
 
-class Test:
+class TestSuite:
     _name=''
-    _totalTests=0
+    _testsRun = False
     _passedTests=0
     _failedTests=0
+    _tests = []
 
     def __init__(self, name):
+        self._testsRun = False
+        self._passedTests=0
+        self._failedTests=0
+        self._tests = []
         self._name = name
         print("Running Tests for: %s." % (name))
 
-    def test(self, name, value):
-        self._totalTests += 1
-        printVal = 'Pass' if value else 'Fail'
-        print(" (%d) %s -> %s" % (self._totalTests, name, printVal))
-        if value:
-            self._passedTests += 1
-        else:
-            self._failedTests += 1
-        return value
+    def addTest(self, test):
+        self._tests.append(test)
+
+    def runTests(self):
+        self._testsRun = True
+        i = 0
+        for test in self._tests:
+            i += 1
+            name = test.getName()
+            value = test.test()
+            printValue = 'Pass' if value else 'Fail'
+            print(" (%d) %s -> %s" % (i, name, printValue))
+            if value:
+                self._passedTests += 1
+            else:
+                self._failedTests += 1
 
     def showReport(self):
+        if not self._testsRun:
+            self.runTests()
         print("Report:")
-        print("  Total Test: %d" % (self._totalTests))
-        print("  Passing Tests: %d, Rate: %.2f%%" % (self._passedTests, ((self._passedTests / self._totalTests) * 100)))
-        print("  Failing Tests: %d, Rate: %.2f%%" % (self._failedTests, ((self._failedTests / self._totalTests) * 100)))
+        print("  Total Test: %d" % (len(self._tests)))
+        print("  Passing Tests: %d, Rate: %.2f%%" %
+              (self._passedTests, ((self._passedTests / float(len(self._tests)))) * 100))
+        print("  Failing Tests: %d, Rate: %.2f%%" %
+              (self._failedTests, ((self._failedTests / float(len(self._tests)))) * 100))
+
+class TestCase:
+    def __init__(self, testSuite, name, *args, **kwargs):
+        self._name = name
+        self._args = args
+        self._kwargs = kwargs
+        testSuite.addTest(self)
+
+    def __call__(self, function):
+        self._function = function
+        return function
+
+    def test(self):
+        return self._function(*self._args, **self._kwargs)
+
+    def getName(self):
+        return self._name
