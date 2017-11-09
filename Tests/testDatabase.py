764b291ce8130e2816fc06636d28bfb3ff5d75bd
Samuel W. Flint <swflint@flintfam.org>
2017-11-09 09:27:53 -0600

    And updated the test cases to use the new testing API

--- Tests/testDatabase.py
+++ Tests/testDatabase.py
@@ -2,40 +2,74 @@
 # coding: utf-8
 
 from Database import Database
-from Test import Test
+from Test import TestSuite, TestCase
 from os import urandom
 from binascii import hexlify
 
 myDB = Database('cbass_test', 'localhost', 'swflint', 'swflint')
 
-auth = Test("User Authentication")
-auth.test("Correct Password", myDB.authenticateUser('test', 'fooBar'))
-auth.test("Incorrect Password", not myDB.authenticateUser('test', 'moo'))
-# auth.test("Invalid Username", not myDB.authenticateUser('cow', 'pig'))
+auth = TestSuite("User Authentication")
+
+@TestCase(auth, "Correct Password")
+def testCorrectPassword():
+    return myDB.authenticateUser('test', 'fooBar')
+
+@TestCase(auth, "Incorrect Password")
+def testIncorrectPassword():
+    return not myDB.authenticateUser('test', 'moo')
+
+@TestCase(auth, "Invalid Username")
+def testInvalidUsername():
+    try:
+        return myDB.authenticateUser('cow', 'pig')
+    except:
+        return False
+
 auth.showReport()
 
 print("")
 
-userManip = Test("User Manipulation")
+userManip = TestSuite("User Manipulation")
 uname = hexlify(urandom(8))
 pword = hexlify(urandom(8))
 email = hexlify(urandom(16))
-myDB.createUser(uname, email, pword)
-userManip.test("Insert User (by verifying existence)", myDB.authenticateUser(uname, pword))
 newPword = hexlify(urandom(8))
-myDB.updatePassword(uname, newPword)
-userManip.test("Change Password (by verifying new validity)", myDB.authenticateUser(uname, newPword))
+
+@TestCase(userManip, "User Insertion (by existence verification)", uname, pword, email)
+def testUserInsertion(user, password, email):
+    myDB.createUser(user, email, password)
+    return myDB.authenticateUser(user, password)
+
+@TestCase(userManip, "Change Password (by validity verification)", uname, newPword)
+def testUserPasswordChange(user, password):
+    myDB.updatePassword(user, password)
+    return myDB.authenticateUser(user, password)
+
 userManip.showReport()
 
 print("")
 
-surveyContents = Test("Survey Data and Contents")
-name = myDB.getSurveyName(1)
-surveyContents.test("Check Name", name == "Test Survey")
-dataTestDict = {
-    'before_text': 'Test Text Before',
-    'after_text': 'Test After Text'
-}
-theDict = myDB.getSurveyProperties(0)
-surveyContents.test("Check Properties", dataTestDict == theDict)
-# And add the rest of the DB tests
+surveyContents = TestSuite("Survey Data and Contents")
+@TestCase(surveyContents, "Returns Correct Name")
+def testNameOfSurvey():
+    return "Check Name" == myDB.getSurveyName(1)
+
+@TestCase(surveyContents, "Returns Correct Properties")
+def testSurveyProperties():
+    testDictionary = {
+        'before_text': 'Test Text Before',
+        'after_text': 'Test After Text'
+    }
+    dictFromDB = myDB.getSurveyProperties(0)
+    for key in dictFromDB.keys():
+        if not key in testDictionary.keys():
+            return False
+    for key in testDictionary.keys():
+        if not key in dictFromDB.keys():
+            return False
+    for key in dictFromDB.keys():
+        if dictFromDB[key] != testDictionary[key]:
+            return False
+    return True
+
+surveyContents.showReport()
