ab50fbf1105ef9c199ed7f241eab47a52eb46e47
Samuel W. Flint <swflint@flintfam.org>
2017-11-10 14:49:50 -0600

    Updated some test stuff

--- Tests/testDatabase.py
+++ Tests/testDatabase.py
@@ -10,65 +10,69 @@ myDB = Database('cbass_test', 'localhost', 'swflint', 'swflint')
 
 auth = TestSuite("User Authentication")
 
-@TestCase(auth, "Correct Password")
-def testCorrectPassword():
+@TestCase(auth, "Authentication W/ Valid Password")
+def testCorrectPassword(printer):
     return myDB.authenticateUser('test', 'fooBar')
 
-@TestCase(auth, "Incorrect Password")
-def testIncorrectPassword():
+@TestCase(auth, "Authentication W/ Invalid Password")
+def testIncorrectPassword(printer):
     return not myDB.authenticateUser('test', 'moo')
 
-@TestCase(auth, "Invalid Username")
-def testInvalidUsername():
+@TestCase(auth, "Authentication W/ Invalid Username")
+def testInvalidUsername(printer):
     try:
         return not myDB.authenticateUser('cow', 'pig')
     except:
+        printer("Raises error.")
         return False
 
-auth.showReport()
-
-print("")
-
-userManip = TestSuite("User Manipulation")
 uname = hexlify(urandom(8))
 pword = hexlify(urandom(8))
 email = hexlify(urandom(16))
 newPword = hexlify(urandom(8))
 
-@TestCase(userManip, "User Insertion (by existence verification)", uname, pword, email)
-def testUserInsertion(user, password, email):
+@TestCase(auth, "User Creation", uname, pword, email)
+def testUserInsertion(printer, user, password, email):
     myDB.createUser(user, email, password)
     return myDB.authenticateUser(user, password)
 
-@TestCase(userManip, "Change Password (by validity verification)", uname, newPword)
-def testUserPasswordChange(user, password):
+@TestCase(auth, "User password change", uname, newPword)
+def testUserPasswordChange(printer, user, password):
     myDB.updatePassword(user, password)
     return myDB.authenticateUser(user, password)
 
-userManip.showReport()
+    
+auth.showReport()
 
 print("")
 
 surveyContents = TestSuite("Survey Data and Contents")
 @TestCase(surveyContents, "Returns Correct Name")
-def testNameOfSurvey():
-    return "Check Name" == myDB.getSurveyName(1)
+def testNameOfSurvey(printer):
+    name = myDB.getSurveyName(1)
+    value = "Test Survey" == name
+    if not value:
+        printer(type(name))
+    return value
 
 @TestCase(surveyContents, "Returns Correct Properties")
-def testSurveyProperties():
+def testSurveyProperties(printer):
     testDictionary = {
         'before_text': 'Test Text Before',
         'after_text': 'Test After Text'
     }
-    dictFromDB = myDB.getSurveyProperties(0)
+    dictFromDB = myDB.getSurveyProperties(1)
     for key in dictFromDB.keys():
         if not key in testDictionary.keys():
+            printer("%s is not in the known data." % (key))
             return False
     for key in testDictionary.keys():
         if not key in dictFromDB.keys():
+            printer("%s is not in the data from the database." % (key))
             return False
     for key in dictFromDB.keys():
         if dictFromDB[key] != testDictionary[key]:
+            printer("Value for key '%s' in database and dictionary are not the same." % (key))
             return False
     return True
 
