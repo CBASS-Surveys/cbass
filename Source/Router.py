0d050d73a15d2b5fa37db29fea36fe71f3536d84
Samuel W. Flint <swflint@flintfam.org>
2017-11-27 22:38:46 -0600

    Still more

--- Source/Router.py
+++ Source/Router.py
@@ -233,19 +233,19 @@ def save():
 
 
 @app.route('/static/js/<path:path>')
-def send_static(path):
+def send_static_js(path):
     return send_from_directory('static/js', path)
 
 @app.route('/static/css/<path:path>')
-def send_static(path):
+def send_static_css(path):
     return send_from_directory('static/css', path)
 
 @app.route('/static/libs/<path:path>')
-def send_static(path):
+def send_static_libs(path):
     return send_from_directory('static/libs', path)
 
 @app.route('/static/media/<path:path>')
-def send_static(path):
+def send_static_media(path):
     return send_from_directory('static/media', path)
     
 @app.route('/static/<path:path>')
