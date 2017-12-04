e4efee0d37af774ba5781d6cb1eba6bb60e378fd
David Rupert Carlson <davidrcarlson29@gmail.com>
2017-12-03 20:39:42 -0600

    (possibly) Finished JSON exporter, added it to Router.py

--- Source/SurveyDataRetrievalController.py
+++ Source/SurveyDataRetrievalController.py
@@ -23,12 +23,12 @@ class SurveyDataRetrievalController:
         #dump(hostname, username, password)
         self._database = Database(db, hostname, username, password)
 
-	def get_survey(self, survey_id):
-		return self._database.getSurveyName(survey_id)
+	def get_survey(self):
+		return self._database.getSurveyName(self.survey_id)
 		
-	def get_questions(self, survey_id):
+	def get_questions(self):
 		survey_questions = []
-		cursor = self._database.getSurveyQuestions(survey_id)
+		cursor = self._database.getSurveyQuestions(self.survey_id)
         question_ids = cursor.fetchall()
         cursor.close()
         for (qId,) in question_ids:
@@ -50,24 +50,24 @@ class SurveyDataRetrievalController:
 
     def get_response_to_questions(self, response_id):
 		responses = []
-        survey_questions = self.get_questions(survey_id)
+        survey_questions = self.get_questions(self.survey_id)
         for question.question_id in survey_questions:	#is this the correct way to retrieve question_id?
             resp = self._database.getIndividualResponseToQuestion(question.question_id, response_id)
             responses += [resp]
         return responses
 		
-	def get_list_of_survey_responses(self, survey_id):
+	def get_list_of_survey_responses(self):
 		survey_answers = []	#a list of lists - each list contains all of each respondent's answers to the survey questions
-		cursor = self._database.getResponsesToSurvey(survey_id)
+		cursor = self._database.getResponsesToSurvey(self.survey_id)
 		for resp_id in cursor.fetchall():
             answers = self.get_responses_to_questions(resp_id)
 			survey_answers += [answers]
         cursor.close()
 		return survey_answers
 	
-    def export_json(self, survey_id):
-		responses_to_survey = self._database.getResponsesToSurvey(survey_id)
-		survey_questions = self.get_questions(survey_id)
+    def export_json(self):
+		responses_to_survey = self._database.getResponsesToSurvey(self.survey_id)
+		survey_questions = self.get_questions(self.survey_id)
 		json_response = []
 		for resp in responses_to_survey:
 			response = {}
@@ -75,24 +75,24 @@ class SurveyDataRetrievalController:
 				response[quest.question_id] = self._database.getIndividualResponseToQuestion(quest.question_id, resp)
 			json_responses += [
 				{"response_id": resp, "response": response}]
-		json_full = {"survey_id": survey_id, "responses": json_responses}
-		return json.dumps(json_full)
+		json_full = {"survey_id": self.survey_id, "responses": json_responses}
+		return json_full
 		
 	
-    def export_csv(self, survey_id):
-		responses_to_survey = self.getResponsesToSurvey(survey_id) 
-        survey_questions = self.get_questions(survey_id)
+    def export_csv(self):
+		responses_to_survey = self.getResponsesToSurvey(self.survey_id) 
+        survey_questions = self.get_questions(self.survey_id)
 		#next: csv format output for all responses
 	
-    def export_excel(self, survey_id):
-		responses_to_survey = self.getResponsesToSurvey(survey_id) 
-        survey_questions = self.get_questions(survey_id)
+    def export_excel(self):
+		responses_to_survey = self.getResponsesToSurvey(self.survey_id) 
+        survey_questions = self.get_questions(self.survey_id)
 		#next: excel format output for all responses
 		#use xlwt package for this?
 
-	def export_sql(self, survey_id):
-		responses_to_survey = self.getResponsesToSurvey(survey_id) 
-        survey_questions = self.get_questions(survey_id)
+	def export_sql(self):
+		responses_to_survey = self.getResponsesToSurvey(self.survey_id) 
+        survey_questions = self.get_questions(self.survey_id)
 		#next: sql format output for all responses
 
 
